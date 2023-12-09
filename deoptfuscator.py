86ccb786903230017eb671b4583dc00c5c4ce21b
graysuit <54432616+graysuit@users.noreply.github.com>
2023-12-09 17:14:18 +0500

    Update deoptfuscator.py

--- deoptfuscator.py
+++ deoptfuscator.py
@@ -11,7 +11,7 @@ def download_file(url, file_name):
                 f.write(chunk)
     return file_name
 
-os.environ['ANDROID_HOST_OUT'] = os.getcwd()
+os.environ['ANDROID_HOST_OUT'] = os.path.join(os.getcwd(), "android")
 APK = "https://d-06.winudf.com/b/APK/Y29tLndlby5wcm9qZWN0el81XzZiZmZkNGJm?_fn=SkFDTyAtINis2KfZg9mIXzEuMC4wX0Fwa3B1cmUuYXBr&_p=Y29tLndlby5wcm9qZWN0eg%3D%3D&is_hot=false&k=9b7cff9dcc863fe4244dda214d8abb996575a260"
 apk_name = download_file(APK, "jaco.1.0.0.0.apk")
 outpath = apk_name.replace(".apk", "de.apk")
