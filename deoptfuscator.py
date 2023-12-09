21b501d0870e89dda4b4c4a529fae83e18355f61
graysuit <54432616+graysuit@users.noreply.github.com>
2023-12-09 16:12:10 +0500

    Update deoptfuscator.py

--- deoptfuscator.py
+++ deoptfuscator.py
@@ -20,20 +20,20 @@ outpath = apk_name.replace(".apk", "de.apk")
 tmp = outpath.split("/")[-1]
 outpath = outpath.replace(tmp, "")
 os.system("rm -rf .apk .std* .profile meta")
-os.system("java -jar apktool.jar d -r -s " + apk_name  + " -o .apk")
+os.system("java -jar tools/apktool.jar d -r -s " + apk_name  + " -o .apk")
 dex_li = [a for a in os.listdir(".apk") if a.endswith(".dex") and a.startswith("classes")]
 os.mkdir(".apk/const")
 #os.makedirs(outpath, exist_ok=True)
 
 for dex in dex_li:
 	deobfuscator.main(".apk/"+dex)
-	os.system("$TOOLS/redex-all -c $TOOLS/default.config .apk/const/const.dex -o .apk/const")
-	print("$TOOLS/redex-all .apk/const/const.dex -o .apk/const")
+	os.system("tools/redex-all -c tools/default.config .apk/const/const.dex -o .apk/const")
+	print("tools/redex-all .apk/const/const.dex -o .apk/const")
 	os.system("mv .apk/const/classes.dex .apk/"+dex)
 
 apk_name = apk_name.replace(".apk", "_deobfuscated.apk")
 apk_name = os.path.basename(apk_name)
-os.system("java -jar $TOOLS/apktool.jar b ./.apk -o " + apk_name)
+os.system("java -jar tools/apktool.jar b ./.apk -o " + apk_name)
 
 os.system("zipalign -f -v 4 " + apk_name + " " + apk_name.replace(".apk", "_align.apk"))
 os.system("apksigner sign --ks deoptfuscator.keystore --ks-pass pass:123456 " + apk_name.replace(".apk", "_align.apk"))
